49301c048b3587c64ffd34b69ed76364ce415681
Taira Nakajima <nakag@ei.tohoku.ac.jp>
2016-11-15 10:07:55 +0900

    Initial Commit

---
+++ PlayAndSendFramePr/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PlayAndSendFramePr
+//
+//  Created by nakajimalab on 2016/11/15.
+//  Copyright © 2016 nakajimalab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
