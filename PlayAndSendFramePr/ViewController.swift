98af48b72d7f0b3fadc12abe27dc7433d2e545ef
Taira Nakajima <nakag@ei.tohoku.ac.jp>
2016-11-15 10:40:59 +0900

    Initial Commit: play movie at http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8

--- PlayAndSendFramePr/ViewController.swift
+++ PlayAndSendFramePr/ViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import Cocoa
+import AVKit
+import AVFoundation
 
 class ViewController: NSViewController {
 
+    @IBOutlet var playerView: AVPlayerView!
+    
+    var videoPlayer: AVPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        let urlString = "http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8"
+        let fileURL = URL(string:urlString)!
+        
+        let avAsset = AVURLAsset(url: fileURL , options: nil)
+        
+        let playerItem = AVPlayerItem(asset: avAsset)
+        videoPlayer = AVPlayer(playerItem: playerItem)
+        
+        playerView.player = videoPlayer
+
     }
 
+    override func viewWillAppear() {
+        super.viewWillAppear()
+        videoPlayer.pause()
+    }
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
